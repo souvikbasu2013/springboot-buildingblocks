31e4252c5156958e78afdbf901f1e39427e826c1
souvikbasu2013 <bunty.nil@gmail.com>
2023-03-11 00:53:11 +0530

    1st commit

---
+++ src/main/java/com/tiptostartmycode/restservices/SpringbootBuildingblockApplication.java
@@ -0,0 +1,14 @@
+package com.tiptostartmycode.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblockApplication.class, args);
+		// start new spring boot project
+	}
+
+}
